2035cfb1d31ace4402558452258e6b4f24b7bf87
Enzo <enzosousasantos08@gmail.com>
2026-08-13 23:49:39 -0300

    melhorei sistemas de armas com objetos

--- armas.py
+++ armas.py
@@ -13,10 +13,9 @@ class Arma:
         acerto = random.randint(1, 100)
         if acerto <= self.chance_acerto:
             dano = random.randint(self.dano_min, self.dano_max)
-            print(f"\nVocê acertou a criatura com a {self.nome} e causou {dano} de dano!")
             alvo.receber_dano(dano)
-        else:
-            print(f"\nVocê errou o ataque com a {self.nome}!")
+            return dano
+        return 0
 faca = Arma("faca", dano_min=10, dano_max=20, chance_acerto=80)
 
 pistola = Arma("pistola", dano_min=25, dano_max=40, chance_acerto=70)
